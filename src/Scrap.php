1038cfbb4eb1d22345517e341c6f4b77e2c631a6
William Mouchon <90479720+Mitsuya77@users.noreply.github.com>
2023-07-05 15:57:15 +0200

    first version

---
+++ src/Scrap.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Wmouc\IimTd3;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Scrap
+{
+    public function getValorantAgents(): array
+    {
+        $url = 'https://playvalorant.com/fr-fr/agents/';
+        // Récupérer le contenu HTML de la page
+        $html = file_get_contents($url);
+
+        // Créer une instance de Crawler
+        $crawler = new Crawler($html);
+
+        // Utiliser les sélecteurs CSS pour extraire les nom des agents
+        $valoAgents = $crawler->filter('.heading-07')->each(function (Crawler $node, $i){
+            return $node->text();
+        });
+
+        return $valoAgents;
+    }
+}
---
+++ tests/scrappeTest.php
@@ -0,0 +1,4 @@
+<?php
+
+use Wmouc\IimOpenSourceTd2\Api;
+use PHPUnit\Framework\TestCase;
